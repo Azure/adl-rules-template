24f9e6691fe0b1597ab413a5438e06e6e1382c10
Nelson Troncoso Aldas <nd.ta@hotmail.com>
2020-07-14 13:08:38 -0400

    updated package.json

--- samples/require-read-only-properties.ts
+++ samples/require-read-only-properties.ts
@@ -5,7 +5,6 @@ export default <Rule>{
     severity: 'error',
     description: 'A model property cannot be both readOnly and required. A readOnly property is something that the server sets when returning the model object while required is a property to be set when sending it as a part of the request body.',
     documentationUrl: 'https://github.com/Azure/azure-rest-api-specs/blob/master/documentation/openapi-authoring-automated-guidelines.md#r2056-requiredreadonlyproperties',
-
   },
   onProperty: (model, property) => {
     if (property.readOnly && property.required) {
